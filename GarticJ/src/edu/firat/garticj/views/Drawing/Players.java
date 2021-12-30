d4157706e03629c7d791136e5617f6dfa84c5f31
Şükrü Taha Bıyık <57368706+ultimate-dev@users.noreply.github.com>
2021-12-30 03:13:06 +0300

    Update Players.java

--- GarticJ/src/edu/firat/garticj/views/Drawing/Players.java
+++ GarticJ/src/edu/firat/garticj/views/Drawing/Players.java
@@ -11,37 +11,50 @@ public class Players extends JPanel {
 
     Player[] players;
     static Players single_instance;
-    public ArrayList<PlayerData> playerDataArrayList  ;
+    public ArrayList<PlayerData> playerDataArrayList;
     Area area;
 
     private Players() {
         setBackground(Color.decode("#7859d1"));
         setPreferredSize(new Dimension(220, getSize().height));
         setMinimumSize(new Dimension(220, getSize().height));
-        setBorder(new EmptyBorder(20,0,0,0));
+        setBorder(new EmptyBorder(20, 0, 0, 0));
         playerDataArrayList = new ArrayList<>();
         area = new Area();
     }
 
-    public static Players getInstance(){
-        if (single_instance ==null){
+    public static Players getInstance() {
+        if (single_instance == null) {
             single_instance = new Players();
         }
         return single_instance;
     }
 
-    public void updatePlayers(){
+    public void clearPlayers() {
+        if (players != null)
+            for (Player player : players) {
+                area.container.remove(player);
+            }
+    }
+
+    public void updatePlayers() {
+        clearPlayers();
 
         players = new Player[playerDataArrayList.size()];
-        int index=0;
 
-        for(PlayerData data:playerDataArrayList){
-            System.out.println("Players "+data.nickname);
-            players[index] = new Player(index+1,data.nickname,data.point);
+        int index = 0;
+        for (PlayerData data : playerDataArrayList) {
+            System.out.println("Players " + data.nickname);
+            players[index] = new Player(index + 1, data.nickname, data.point);
             area.container.add(players[index]);
             index++;
         }
+        area.container.revalidate();
         area.container.repaint();
+        area.revalidate();
+        area.repaint();
+        revalidate();
+        repaint();
         add(area);
     }
 
@@ -56,7 +69,7 @@ public class Players extends JPanel {
             setPreferredSize(new Dimension(220, 70));
             setLayout(null);
 
-            orderComponent = new JLabel(order+"");
+            orderComponent = new JLabel(order + "");
             orderComponent.setBounds(16, 20, 16, 16);
             orderComponent.setBackground(Color.decode("#7859d1"));
             orderComponent.setFont(new Font("Serif", Font.BOLD, 16));
@@ -65,7 +78,7 @@ public class Players extends JPanel {
             add(orderComponent);
 
 
-            pointComponent = new JLabel(point+"p");
+            pointComponent = new JLabel(point + "p");
             pointComponent.setBounds(40, 5, 50, 50);
             pointComponent.setBackground(Color.decode("#937ada"));
             pointComponent.setOpaque(true);
